64b2debcef07b5abfc8ad9d97fba2087f2fdce39
Qiyuan Cui <cuiqiyuan1989@gmail.com>
2023-03-27 23:25:48 -0500

    entity

---
+++ src/main/java/com/restpai/entity/Comment.java
@@ -0,0 +1,13 @@
+package com.restpai.entity;
+
+public class Comment {
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String body;
+
+    private Post post;
+}
