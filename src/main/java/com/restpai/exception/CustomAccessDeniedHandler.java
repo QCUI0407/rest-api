8c9e99ef0c6561a00760b491f014bfa9f911a02c
Qiyuan Cui <cuiqiyuan1989@gmail.com>
2023-05-01 08:01:42 -0500

    exception

---
+++ src/main/java/com/restpai/exception/CustomAccessDeniedHandler.java
@@ -0,0 +1,27 @@
+package com.restpai.exception;
+import com.restpai.utils.BaseResponseDTO;
+import com.restpai.utils.HelperUtils;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        BaseResponseDTO responseDTO = new BaseResponseDTO();
+        responseDTO.setMessage("You don't have permission to access this resource");
+        responseDTO.setCode(String.valueOf(HttpStatus.FORBIDDEN.value()));
+
+        String json = HelperUtils.JSON_WRITER.writeValueAsString(responseDTO);
+
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(json);
+    }
+}
